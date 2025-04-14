0fa8a6c4db22a5fa54d29b6e3ee157f5d109f84f
Lucas Muto <lucasmuto@id.uff.br>
2025-04-14 17:53:03 -0300

    feat: implementação da rota GET /items

--- src/routes/items.routes.js
+++ src/routes/items.routes.js
@@ -6,8 +6,10 @@ const router = express.Router();
 // POST /items - Criar um novo item
 router.post('/', itemsController.create);
 
-// Rotas serão implementadas em etapas posteriores
 // GET /items - Listar todos os itens, com filtro opcional por tipo
+router.get('/', itemsController.getAll);
+
+// Rotas serão implementadas em etapas posteriores
 // GET /items/:id - Buscar um item específico por ID
 
 export default router; 
